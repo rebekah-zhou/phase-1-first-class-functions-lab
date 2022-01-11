95426f813f5d7ad9c770cd17c05abb7deb87df5a
Rebekah Zhou <rebekahzhou@gmail.com>
2022-01-10 19:01:30 -0800

    commit tree 804a3dee42d591da3677070c7f5f81af016de6eb

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayDrivers) {
+    return arrayDrivers.slice(0,2);
+} 
+
+const returnLastTwoDrivers = function(arrayDrivers) {
+    return arrayDrivers.slice(-2);
+} 
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function(fare) {
+        return num*fare;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers);
+}
